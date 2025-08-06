518efe0779407ce31d2ba5d509d16a4b5c081fb5
Reshzera <reoshiro13@gmail.com>
2025-08-06 11:04:04 -0400

    feat: add SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), vue()],
+  adapter: netlify(),
 });
